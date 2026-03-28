afe576a031e04ef239700e2b1fe5cff6bc182b22
alunosg <alunosg.altodalapa@gmail.com>
2026-03-28 12:24:35 -0300

    Player Animations

--- DAVI NIVEL GAMEDEV 1/Assets/Scripts/PlayerController.cs
+++ DAVI NIVEL GAMEDEV 1/Assets/Scripts/PlayerController.cs
@@ -8,28 +8,50 @@ public class PlayerController : MonoBehaviour
     public float speed = 10;
     public float jumpForce = 10;
     public LayerMask floorLayer;
+    public Animator anim;
+    private bool grounded = false;
 
         private Vector2 moveInput;
 
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        anim.SetBool("Ismoving",moveInput.x != 0);
     }
-    
+
     public void Jump(InputAction.CallbackContext context)
     {
-        Vector2 leftPoint = new(col.bounds.min.x, col.bounds.max.y);
-        Vector2 rightPoint = new(col.bounds.max.x,col.bounds.max.y);
-
-            if(Physics2D.Raycast(leftPoint, Vector2.down, col.bounds.size.y * 1.1f, floorLayer) ||
-                Physics2D.Raycast(rightPoint, Vector2.down, col.bounds.size.y * 1.1f, floorLayer))
-        {
+        if (context.started && grounded)
+      { anim.SetTrigger("Jump");
             rig.linearVelocity = new(rig.linearVelocity.x, jumpForce);
+        }   
+    }    
+
+
+    private void GroundCheck()
+    {
+        Vector2 leftPoint = new(col.bounds.min.x, col.bounds.max.y);
+        Vector2 rightPoint = new(col.bounds.max.x, col.bounds.max.y);
+
+        if (Physics2D.Raycast(leftPoint, Vector2.down, col.bounds.size.y * 1.1f, floorLayer) ||
+            Physics2D.Raycast(rightPoint, Vector2.down, col.bounds.size.y * 1.1f, floorLayer))
+        {
+            grounded = true;
         }
+        else
+        {
+            grounded = false;
+
+        }
+
+        anim.SetBool("IsGrounded", grounded);
     }
+
+
 
     public void FixedUpdate()
     {
+        GroundCheck();
         rig.linearVelocity = new Vector2(moveInput.x * speed, rig.linearVelocity.y);
     }
 
